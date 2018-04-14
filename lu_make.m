6ab082c31064335d7731f574ff64e3a2be7e39bb
choiwb <md4595@naver.com>
2018-04-14 17:26:42 +0900

    commit

---
+++ lu_make.m
@@ -0,0 +1,11 @@
+function [L U] = lu_make(A)
+
+[n m] = size(A);
+U = zeros(n,m); L = eye(n);
+for k = 1:n
+    U(k,k) = A(k,k) -L(k, 1:k-1)*U(1:k-1,k);
+    for j = k+1:n
+        U(k,j) = A(k,j) - L(k, 1:k-1)*U(1:k-1,j);
+        L(j,k) = (A(j,k) - L(j, 1:k-1)*U(1:k-1, k))/U(k,k);
+    end
+end
