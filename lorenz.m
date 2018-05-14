853bc98536f4fe2360bb71325f9127751dcc1ef2
choiwb <md4595@naver.com>
2018-05-14 18:57:28 +0900

    commit

--- lorenz.m
+++ lorenz.m
@@ -3,7 +3,9 @@
 % 'a' is array containing x, y, and z variables
 % 't' is time variable
 
-sigma = 10;
+% sigma = 10;
+sigma = 14;
+
 beta = 8/3;
 rho = 28;
 f = @(t,a) [-sigma*a(1) + sigma*a(2); rho*a(1) - a(2) - a(1)*a(3); -beta*a(3) + a(1)*a(2)];
@@ -12,6 +14,8 @@ f = @(t,a) [-sigma*a(1) + sigma*a(2); rho*a(1) - a(2) - a(1)*a(3); -beta*a(3) + 
 % [t,a] = ode45(f,[0 100],[5 5 5]);     % Runge-Kutta 4th/5th order ODE solver
 [t,a] = ode45(f,[0 100],[5.001 5 5]);     % Runge-Kutta 4th/5th order ODE solver
 
+figure(1)
 plot3(a(:,1),a(:,2),a(:,3))
 xlabel('x');ylabel('y');zlabel('z');grid
+
 
