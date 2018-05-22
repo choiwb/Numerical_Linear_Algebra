ae050e5a639d32ac3c1fe63ae9307c6e82e821d5
choiwb <md4595@naver.com>
2018-05-22 10:49:12 +0900

    commit

--- lorenz.m
+++ lorenz.m
@@ -1,4 +1,4 @@
-sigma = 10; beta = 8/3; rho = 228;
+sigma = 10; beta = 8/3; rho = 28;
 f = @(t,a) [-sigma*a(1) + sigma*a(2); rho*a(1) - a(2) - a(1)*a(3); -beta*a(3) + a(1)*a(2)];
 [t,a] = ode45(f,[0 100], [1 1 1]);
 
